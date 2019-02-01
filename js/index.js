d6403212b219710f854dcec0d65c4cab802fdaac
DWL716 <1239787435@qq.com>
2019-02-01 22:43:32 +0800

    sse

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
 
-/* var aside3 = document.getElementById("aside3");
-var asiden = document.getElementById("asiden");
-asiden.click(function() {
 
-}) */
+ window.onload=function(){
+    var obt=document.getElementById("asiden");
+    var odiv=document.getElementById("ul1");
+    obt.onclick=function(){
+      if(odiv.style.display == "none"){
+        odiv.style.display = "block";
+      }
+      else{
+        odiv.style.display = "none";
+      }
+    }
+  } 
