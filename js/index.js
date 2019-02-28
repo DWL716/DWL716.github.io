1df11acdc901cf0e807c1852c8c7a280d25b09a7
DWL716 <1239787435@qq.com>
2019-02-28 13:50:19 +0800

    m12

--- js/index.js
+++ js/index.js
@@ -1,14 +1,31 @@
 
 
- window.onload=function(){
+ (function(){
     var obt=document.getElementById("asiden");
     var odiv=document.getElementById("ul1");
-    obt.onclick=function(){
+  
+    obt.onclick=ck;
+    function ck(){
       if(odiv.style.display == "none"){
         odiv.style.display = "block";
+        console.log(11)
       }
       else{
         odiv.style.display = "none";
       }
     }
-  } 
+   
+      var tops = document.getElementById("top");
+      window.onscroll = function(){
+          var topScrollbar = document.documentElement.scrollTop;
+          if(topScrollbar > 30) {
+            tops.style.bottom = "15%"
+          }else{
+            tops.style.bottom = "-5%"
+          }
+      }
+    
+
+  ck();
+  })()
+  
